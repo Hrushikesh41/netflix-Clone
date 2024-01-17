4a7b7cc2acf4bec7a742f127b89fd608822b595b
Hrushikesh41 <hrushikesh.kok@gmail.com>
2024-01-17 09:57:22 +0530

    Created Server 👨🏻‍💻

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const env = require('dotenv');
+
+const app = express();
+app.use(express.json());
+
+env.config({path : 'config.env'});
+
+const port = process.env.PORT || 3000;
+
+app.post("/", (req, res) => {
+    res.send("Hello Streamers")
+});
+
+app.listen(port, () => {
+    console.log("APP LISTENING AT PORT : " + port);
+});
